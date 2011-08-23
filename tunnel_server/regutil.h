694867a37c13925bfcf731b94195b6accb522b93
Carlos D. Garza <carlos.garza@rackspace.com>
2011-08-23 16:58:34 -0500

    Adding highlevel pcre regex objects

--- tunnel_server/regutil.h
+++ tunnel_server/regutil.h
@@ -1,2 +1,20 @@
+#include<pcre.h>
+
+#ifndef __REGUTIL_H
+#define __REGUTIL_H
+
+typedef struct{
+    pcre *p;
+    int maxgroups;
+}re_t;
+
+typedef struct{
+    re_t *re;
+    char *line;
+    int *ov;
+    int ngroups;
+}matcher_t;
+#endif
+
 char *getGroup(int idx,int *ov,char *strIn,int *error);
 int showString(char *str);
